fafce2c4262d65a6ffd9f71d1b07d8b7ca66af53
batestin1 <mayconcipriano@gmail.com>
2023-02-16 10:38:18 -0300

    V6 - 20231602

--- scripts/main.py
+++ scripts/main.py
@@ -15,35 +15,50 @@
 ###################################################################################################
 
 # imports
-from keys import keys, change_excel
+import json
+import requests
+import os
+
+path = "database/"
+
+for filename in os.listdir(path):
+
+    if filename.endswith(".xlsx"):
+
+        if filename != "dataset.xlsx":
+            os.rename(os.path.join(path, filename), os.path.join(path, "dataset.xlsx"))
+            print(f"File {filename} renamed to dataset.xlsx")
+
+        else:
+            print("The file already has the name dataset.xlsx")
+
+    else:
+        print(f"File {filename} is not an Excel file") 
+from keys import keys
 from excel_to_trello import create_board
 from trello_to_excel import to_excel
 from optional.del_board import del_boards
 
 
-if __name__ == "__main__":
-    while True:
-        choice = int(input("""
-        What do you want to do:
-        [1] - Excel To Trello
-        [2] - Trello To Excel
-        [3] - Del Board on Trello
-        R: """))
-        if choice == 1:
-            keys()
-            change_excel()
-            create_board()
-            break
-        elif choice == 2:
-            keys()
-            change_excel()
-            to_excel()
-            break
-        elif choice == 3:
-            keys()
-            change_excel()
-            del_boards()
-            break
-        else:
-            print("Invalid choice. Please choose a valid option.\n")
+while True:
+    choice = int(input("""
+    What do you want to do:
+    [1] - Excel To Trello
+    [2] - Trello To Excel
+    [3] - Del Board on Trello
+    R: """))
+    if choice == 1:
+        keys()
+        create_board()
+        break
+    elif choice == 2:
+        keys()
+        to_excel()
+        break
+    elif choice == 3:
+        keys()
+        del_boards()
+        break
+    else:
+        print("Invalid choice. Please choose a valid option.\n")
 
